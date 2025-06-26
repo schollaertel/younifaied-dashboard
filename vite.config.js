9c0465bc0b2a93683b98422c5f7abbaa5ccd6b79
schollaertel <erinchantz@yahoo.com>
2025-06-26 08:28:48 -0400

    Remove leftover git conflict markers

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-=======
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
->>>>>>> 289c02e702defbe198c2460edef27ea764f648ed
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -22,14 +14,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     allowedHosts: [
-<<<<<<< HEAD
-      '5173-ia9b2j459lmam5k7fsfml-aa04639f.manusvm.computer',
-=======
       '5173-1a9b2j4591mam5k7fsfml-aa04639f.manusvm.computer',
->>>>>>> 289c02e702defbe198c2460edef27ea764f648ed
       'localhost',
       '127.0.0.1'
-    ]
-  }
+    ],
+  },
 })
-
